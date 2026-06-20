128b0cbfcf94fb1fcfc4e9a372cab7d9fe41c0e1
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2026-06-21 00:34:09 +0200

    fix

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 const CANONICAL_HOST = "www.mflabz.com";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const host = request.headers.get("host");
 
   if (
